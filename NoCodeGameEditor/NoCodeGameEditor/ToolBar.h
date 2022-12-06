3a667a44497304ab6b2df74fb4db21bf3dd3ca01
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2022-12-06 11:03:52 +0000

    Began working on toolbar and icons

---
+++ NoCodeGameEditor/NoCodeGameEditor/ToolBar.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class ToolBar
+{
+public:
+	ToolBar();
+
+	void loadFiles();
+
+	void update(sf::Time t_deltaTime, sf::RenderWindow& t_window);
+
+	void render(sf::RenderWindow& t_window);
+
+	void checkForMousePosAndClick(sf::RenderWindow& t_window, sf::Vector2i t_mousePos);
+
+
+private:
+
+	sf::Sprite toolBarSprite;
+	sf::Texture toolBarTexture;
+
+	sf::Sprite brushToolSprite;
+	sf::Texture brushToolTexture;
+
+	sf::Sprite fillToolSprite;
+	sf::Texture fillToolTexture;
+
+};

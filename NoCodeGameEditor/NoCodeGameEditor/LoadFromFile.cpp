9f15ef62233928e365668bed98112937cbda27eb
SeanWhelan117 <57904168+SeanWhelan117@users.noreply.github.com>
2023-03-15 14:07:28 +0000

    working on game loading still

--- NoCodeGameEditor/NoCodeGameEditor/LoadFromFile.cpp
+++ NoCodeGameEditor/NoCodeGameEditor/LoadFromFile.cpp
@@ -12,18 +12,36 @@ void LoadFromFile::loadFile(std::string t_gameName)
 	while (!myFile.eof())
 	{
 		std::string temp;
-		std::cout << temp << std::endl;
+		//std::cout << temp << std::endl;
 		getline(myFile, temp);
 		gameData.push_back(temp);
 	}
-	dataRead = true;
+	
+	std::vector<std::string>::iterator it;
+
+	it = gameData.begin();
+	gameData.erase(it);
+	it = gameData.begin();
+	gameData.erase(it);
+	it = gameData.begin();
+
+	for (int i = 0; i < gameData.size(); i++)
+	{
+		//it++;
+		//if (gameData.at(i) == "")
+		//{
+
+		//}
+		std::cout << gameData.at(i) << std::endl;
+	}
+	//dataRead = true;
 	myFile.close();
 }
 
 void LoadFromFile::update(sf::Time t_deltaTime)
 {
-	if (dataRead == true)
+	/*if (dataRead == true)
 	{
 		gameData;
-	}
+	}*/
 }
